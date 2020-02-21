ee0eaabf7801707956e7c87d24136826e8863b07
Joe Richards <nospam-github@disconformity.net>
2020-02-21 15:10:36 -0800

    Fix C temperature conversion

--- tilt.py
+++ tilt.py
@@ -134,7 +134,7 @@ class Tilt():
     def add_temp(self, temp_f):
         self.temps_f.append(temp_f)
         self.temp_f = mean(self.temps_f)
-        self.temp_c = round((temp_f - 32) * 1.8, 1)
+        self.temp_c = round((temp_f - 32) * 5/9, 1)
         return True
 
     def add_sg(self, sg):
